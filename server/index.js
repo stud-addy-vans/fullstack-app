2494cd78e98ef4771c963cdb5172b920c5fd532b
stud-addy-vans <aditya.22scse1010958@galgotiasuniversity.edu.in>
2026-04-20 23:30:56 +0530

    fix cors issue

--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 app.use(express.json());
 
 // ROUTE
